89f35c6c29058170d32736bcd115e6b6ba54a4ba
Choisooyoung98 <ekfms15815@gmail.com>
2022-07-15 13:36:35 +0900

    2단계

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -8,8 +8,23 @@ public class App {
     void run(){
         System.out.println("=== 명언 SSG ===");
 
-        System.out.print("명령) ");
-        String cmd = sc.nextLine().trim();
+        outer:
+        while(true) {
+            System.out.print("명령) ");
+            String cmd = sc.nextLine().trim();
+
+            switch (cmd) {
+                case "등록":
+                    System.out.print("명언 : ");
+                    String content = sc.nextLine().trim();
+                    System.out.print("작가 : ");
+                    String author = sc.nextLine().trim();
+                    continue;
+                case "종료":
+                    break outer;
+
+            }
+        }
 
     }
 }
