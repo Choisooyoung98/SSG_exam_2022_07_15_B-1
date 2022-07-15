2cf7ab51a843323c7d3b8d696204284fb0122d1a
Choisooyoung98 <ekfms15815@gmail.com>
2022-07-15 13:39:55 +0900

    3, 4단계

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class App {
     Scanner sc = new Scanner(System.in);
 
+    int wiseSayingLastId = 0;
     void run(){
         System.out.println("=== 명언 SSG ===");
 
@@ -15,14 +16,15 @@ public class App {
 
             switch (cmd) {
                 case "등록":
+                    ++wiseSayingLastId;
                     System.out.print("명언 : ");
                     String content = sc.nextLine().trim();
                     System.out.print("작가 : ");
                     String author = sc.nextLine().trim();
+                    System.out.println(wiseSayingLastId + "번 명언이 등록되었습니다.");
                     continue;
                 case "종료":
                     break outer;
-
             }
         }
 
