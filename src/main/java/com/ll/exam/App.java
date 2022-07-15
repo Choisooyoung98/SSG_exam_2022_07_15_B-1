db0ad713959bdb55923fd4b6b29cb3512c525254
Choisooyoung98 <ekfms15815@gmail.com>
2022-07-15 14:33:19 +0900

    5단계

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,11 +1,13 @@
 package com.ll.exam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     Scanner sc = new Scanner(System.in);
-
     int wiseSayingLastId = 0;
+    List<WiseSaying> wiseSayings = new ArrayList<>();
     void run(){
         System.out.println("=== 명언 SSG ===");
 
@@ -13,8 +15,8 @@ public class App {
         while(true) {
             System.out.print("명령) ");
             String cmd = sc.nextLine().trim();
-
-            switch (cmd) {
+            Rq rq = new Rq(cmd);
+            switch (rq.getPath()) {
                 case "등록":
                     ++wiseSayingLastId;
                     System.out.print("명언 : ");
@@ -22,7 +24,19 @@ public class App {
                     System.out.print("작가 : ");
                     String author = sc.nextLine().trim();
                     System.out.println(wiseSayingLastId + "번 명언이 등록되었습니다.");
+                    WiseSaying wiseSaying = new WiseSaying(wiseSayingLastId, content, author);
+                    wiseSayings.add(wiseSaying);
                     continue;
+                case "목록":
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("--------------------");
+                    for (int i = 0; i < wiseSayings.size(); i++){
+                        WiseSaying wiseSaying_ = new WiseSaying();
+                        wiseSaying_ = wiseSayings.get(i);
+                        System.out.println(wiseSaying_.id + " / " + wiseSaying_.content + " / " + wiseSaying_.author);
+                    }
+                    continue;
+
                 case "종료":
                     break outer;
             }
