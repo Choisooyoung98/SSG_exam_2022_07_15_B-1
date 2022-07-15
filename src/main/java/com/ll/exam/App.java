20456c36493c8e7ca0dd12667f2655ef37b9570f
Choisooyoung98 <ekfms15815@gmail.com>
2022-07-15 13:31:10 +0900

    1단계

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,7 +1,15 @@
 package com.ll.exam;
 
+import java.util.Scanner;
+
 public class App {
+    Scanner sc = new Scanner(System.in);
+
     void run(){
+        System.out.println("=== 명언 SSG ===");
+
+        System.out.print("명령) ");
+        String cmd = sc.nextLine().trim();
 
     }
 }
