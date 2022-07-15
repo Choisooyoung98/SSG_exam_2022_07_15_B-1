d10774c327cdfa0f767d9868322b1efee84b7b7b
Choisooyoung98 <ekfms15815@gmail.com>
2022-07-15 14:55:09 +0900

    수정 도입 8단계 + 리펙토링

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -18,37 +18,16 @@ public class App {
             Rq rq = new Rq(cmd);
             switch (rq.getPath()) {
                 case "등록":
-                    ++wiseSayingLastId;
-                    System.out.print("명언 : ");
-                    String content = sc.nextLine().trim();
-                    System.out.print("작가 : ");
-                    String author = sc.nextLine().trim();
-                    System.out.println(wiseSayingLastId + "번 명언이 등록되었습니다.");
-                    WiseSaying wiseSaying = new WiseSaying(wiseSayingLastId, content, author);
-                    wiseSayings.add(wiseSaying);
+                    write(rq);
                     continue;
                 case "목록":
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("--------------------");
-                    for (int i = wiseSayings.size() - 1; i >= 0; i--) {
-                        WiseSaying wiseSaying_ = new WiseSaying();
-                        wiseSaying_ = wiseSayings.get(i);
-                        System.out.println(wiseSaying_.id + " / " + wiseSaying_.content + " / " + wiseSaying_.author);
-                    }
+                    list(rq);
+                    continue;
+                case "수정":
+                    write(rq);
                     continue;
                 case "삭제":
-                    int paramId = rq.getIntParam("id",0);
-                    if (paramId == 0) {
-                        System.out.println("id를 입력해주세요.");
-                        continue;
-                    }
-                    WiseSaying foundWiseSaying = findById(paramId);
-                    if (foundWiseSaying == null) {
-                        System.out.println(paramId + "번째 명언은 존재하지 안습니다.");
-                        continue;
-                    }
-                    wiseSayings.remove(foundWiseSaying);
-                    System.out.println(paramId + "번째 명언이 삭제되었습니다.");
+                    remove(rq);
                     continue;
                 case "종료":
                     break outer;
@@ -61,5 +40,55 @@ public class App {
                 return wiseSaying;
         }
         return null;
+    }
+    private void write(Rq rq) {
+        ++wiseSayingLastId;
+        System.out.print("명언 : ");
+        String content = sc.nextLine().trim();
+        System.out.print("작가 : ");
+        String author = sc.nextLine().trim();
+        System.out.println(wiseSayingLastId + "번 명언이 등록되었습니다.");
+        WiseSaying wiseSaying = new WiseSaying(wiseSayingLastId, content, author);
+        wiseSayings.add(wiseSaying);
+    }
+    private void list(Rq rq) {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("--------------------");
+        for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+            WiseSaying wiseSaying_ = new WiseSaying();
+            wiseSaying_ = wiseSayings.get(i);
+            System.out.println(wiseSaying_.id + " / " + wiseSaying_.content + " / " + wiseSaying_.author);
+        }
+    }
+    private void modify(Rq rq) {
+        int paramId = rq.getIntParam("id", 0);
+        if (paramId == 0) {
+            System.out.println("id를 입력해주세요.");
+            return;
+        }
+        WiseSaying foundWiseSaying = findById(paramId);
+        if (foundWiseSaying == null) {
+            System.out.println(paramId + "번째 명언은 존재하지 안습니다.");
+            return;
+        }
+        System.out.println("명언(기존) : " + foundWiseSaying.content);
+        foundWiseSaying.content = sc.nextLine().trim();
+        System.out.println("작가(기존) : " + foundWiseSaying.author);
+        foundWiseSaying.author = sc.nextLine().trim();
+        System.out.println(paramId + "번째 명언이 수정되었습니다.");
+    }
+    private void remove(Rq rq) {
+        int paramId = rq.getIntParam("id", 0);
+        if (paramId == 0) {
+            System.out.println("id를 입력해주세요.");
+            return;
+        }
+        WiseSaying foundWiseSaying = findById(paramId);
+        if (foundWiseSaying == null) {
+            System.out.println(paramId + "번째 명언은 존재하지 안습니다.");
+            return;
+        }
+        wiseSayings.remove(foundWiseSaying);
+        System.out.println(paramId + "번째 명언이 삭제되었습니다.");
     }
 }
